c0f9ae84b3d8489f4a3b9e9ed7a9a9a32a6fa049
zahrayazdani <zahra.yazdani@ut.ac.ir>
2019-05-17 12:23:50 +0430

    Add run method

--- Manager.cpp
+++ Manager.cpp
@@ -13,9 +13,17 @@ Manager::Manager()
 	command_handler = new CommandHandler;
 	signup_id = FIRST_ID;
 	film_id = FIRST_ID;
+	active_user = NULL;
 }
 
 void Manager::run()
 {
-	
+	while (true)
+	{
+		// get_command;
+		// control_command;
+		// do_command;
+		// print;
+	}
 }
+
